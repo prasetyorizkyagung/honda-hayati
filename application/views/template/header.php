4785658662f0202e1e8409afa9347f788e14c160
prasetyorizkyagung <prasetyorizkyagung@gmail.com>
2017-09-26 23:34:11 +0700

    Ubah logo header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -25,7 +25,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html"><img src="<?php echo base_url('uploads/logo-HPM.png')?>" width="100px" alt="logo"></a>
+                    <a class="navbar-brand" href="index.html"><img src="<?php echo base_url('uploads/LOGO-HPM.png')?>" width="100px" alt="logo"></a>
                 </div>
 				
                 <div class="collapse navbar-collapse navbar-right">
